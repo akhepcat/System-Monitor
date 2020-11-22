4664fb1f1fe11c3674039277f49e92caa6602461
Leif Sawyer <leif@denali.net>
2020-11-22 00:43:32 -0900

    allow for imagemagick 'convert' instead of pbmtext

--- uptime.sh
+++ uptime.sh
@@ -8,16 +8,24 @@ PROGNAME=${PROG%%.*}
 MYHOST="$(uname -n)"
 CMD="$1"
 GRAPHNAME="${WEBROOT:-.}/${MYHOST}-uptime.gif"
-UPTIME=$(uptime 2>/dev/null)
 
 case ${CMD} in
 	(debug)
 		echo "WEBROOT=${WEBROOT}"
 		echo "GRAPHNAME=${GRAPHNAME}"
-		echo "UPTIME=${UPTIME}"
+		echo "UPTIME=$(uptime)"
 		;;
 	(update|graph|graph-weekly|graph-monthly|graph-yearly)
-		echo ${UPTIME} | pbmtext | pnmcrop | pnmpad -white -left 2 -right 2 -top 2 -bottom 2 | pnmtopng > ${GRAPHNAME}
+		if [ -n "$(which convert)" ]
+		then
+			uptime | convert -size 640x20 -font Liberation-Sans -pointsize 18 -unsharp 0x.5 -background none -trim +repage -fill black text:- ${GRAPHNAME}
+		elif [ -n "$(which pbmtext)" ]
+		then
+			uptime | pbmtext | pnmcrop | pnmpad -white -left 2 -right 2 -top 2 -bottom 2 | pnmtopng > ${GRAPHNAME}
+		else
+			> ${GRAPHNAME}
+			echo "no ImageMagick or NetPBM installed, can't convert uptime to png"
+		fi
 		;;
 	(force-create|create)
 		;;
