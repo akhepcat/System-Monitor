e5c547443e2f0e199c6b0e30f80489df130f7d60
Leif Sawyer <leif@akhepcat.com>
2021-04-02 13:48:20 -0800

    refactor graph building to collapse into single function

--- memory.sh
+++ memory.sh
@@ -10,17 +10,90 @@ PROGNAME=${PROG%%.*}
 CMD="$1"
 MYHOST="$(uname -n)"
 MYHOST=${SERVERNAME:-$MYHOST}
+DATE=$(date)
 
 
 RRDFILE="${RRDLIB:-.}/${MYHOST}-mem.rrd"
-GRAPHNAME="${WEBROOT:-.}/${MYHOST}-mem.png"
+GRAPHBASE="${WEBROOT:-.}/${MYHOST}-mem.png"
+
+DONTRRD=0	#override for now
+
+do_graph() {
+	if [ "${DONTRRD:-0}" = "1" ]
+	then
+		return
+	fi
+	#defaults, overridden where needed
+	SP='\t'	# nominal spacing
+	XAXIS=""	# only the daily graph gets custom x-axis markers
+
+	case $1 in
+		day)
+			GRAPHNAME="${GRAPHBASE}"
+			TITLE="${MYHOST} last 24 hours' data for ${DATE}"
+			START=""
+			# SCALE="--upper-limit 150 --alt-autoscale-min"
+			XAXIS="MINUTE:30:MINUTE:30:HOUR:1:0:%H"
+		;;
+		week)
+			GRAPHNAME="${GRAPHBASE//.png/-week.png}"
+			TITLE="${MYHOST} last 7 days' data for ${DATE}"
+			START="end-$LASTWEEK"
+		;;
+		month)
+	    		GRAPHNAME="${GRAPHBASE//.png/-month.png}"
+			TITLE="${MYHOST} last months' data for ${DATE}"
+	    		START="end-$LASTMONTH"
+	    	;;
+		year)
+	    		GRAPHNAME="${GRAPHBASE//.png/-year.png}"
+			TITLE="${MYHOST} last years' data for ${DATE}"
+	    		START="end-$LASTYEAR"
+	    	;;
+	    	*) 	echo "broken graph call"
+	    		exit 1
+	    	;;
+	esac
+
+	if [ -z "${GRAPHNAME}" ]
+	then
+		echo "graphname is null"
+		echo "graphbase is $GRAPHBASE"
+		exit 1
+	fi
+
+	rrdtool graph ${GRAPHNAME} \
+	        -v "${PROGNAME} Bytes" -w 700 -h 300  -t "${TITLE}" \
+	        --use-nan-for-all-missing-data \
+		--color ARROW\#000000  \
+		${START:+--end now} ${START:+--start $START}  ${XAXIS:+--x-grid $XAXIS} \
+		DEF:usedmem=${RRDFILE}:usedmem:AVERAGE \
+		DEF:usedswap=${RRDFILE}:usedswap:AVERAGE \
+		DEF:totalmem=${RRDFILE}:totalmem:AVERAGE \
+		DEF:totalswap=${RRDFILE}:totalswap:AVERAGE \
+		COMMENT:"${SP}\j" \
+		COMMENT:"${SP}" \
+		AREA:totalmem\#0000FF:"Total phys memory" \
+		AREA:totalswap\#000000:"Total with swap":STACK \
+		AREA:usedmem\#00FF00:"Memory used" \
+		LINE:usedswap\#FF0000:"Swap used":STACK \
+		COMMENT:"${SP}\j" \
+		COMMENT:"${SP}" \
+		GPRINT:usedmem:AVERAGE:"Memory used\: %.0lf%s bytes" \
+		GPRINT:usedswap:AVERAGE:"Swap used\: %.0lf%s bytes" \
+		COMMENT:"${SP}\j" \
+		COMMENT:"${SP}" \
+		GPRINT:totalmem:MAX:"Total memory available\: %.0lf%s bytes" \
+		GPRINT:totalswap:MIN:"Total swap available\: %.0lf%s bytes" \
+		COMMENT:"${SP}\j" 
+}
 
 case $CMD in
 	(debug)
 		echo "RRDLIB=${RRDLIB}"
 		echo "WEBROOT=${WEBROOT}"
 		echo "RRDFILE=${RRDFILE}"
-		echo "GRAPHNAME=${GRAPHNAME}"
+		echo "GRAPHBASE=${GRAPHBASE}"
 		echo N=$(
 			grep -E '^(Mem|Buff|Cache)' /proc/meminfo | \
 			    gawk 'match($1,/^MemTotal:$/) { print $(NF-1)*1024 }; match($1,/^Buffers:$/) { print $(NF-1)*1024 }; match($1,/^Cached:$/) { print $(NF-1)*1024 };' | tr '\n' ':')$(
@@ -63,97 +136,18 @@ case $CMD in
 			    sed  'N; {s/SwapTotal://; s/kB.*SwapFree://; s/kB//;}'| \
 			    gawk '{ print ($1 * 1024) ":" (($1 - $2) * 1024) ":" ($2 * 1024) }')
 		;;
-	(graph|graph-day)
-    rrdtool graph ${GRAPHNAME} \
-		-v "Bytes" -w 700 -h 300 -t "${MYHOST} last 24 hours memory usage- ${DATE}" \
-		-c ARROW\#000000 -x MINUTE:30:MINUTE:30:HOUR:1:0:%H \
-		DEF:usedmem=${RRDFILE}:usedmem:AVERAGE \
-		DEF:usedswap=${RRDFILE}:usedswap:AVERAGE \
-		DEF:totalmem=${RRDFILE}:totalmem:AVERAGE \
-		DEF:totalswap=${RRDFILE}:totalswap:AVERAGE \
-		COMMENT:"	" \
-		AREA:totalmem\#0000FF:"Total phys memory" \
-		AREA:totalswap\#000000:"Total with swap":STACK \
-		AREA:usedmem\#00FF00:"Memory used" \
-		LINE:usedswap\#FF0000:"Swap used":STACK \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:usedmem:AVERAGE:"Memory used\: %.0lf%s bytes" \
-		GPRINT:usedswap:AVERAGE:"Swap used\: %.0lf%s bytes" \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:totalmem:MAX:"Total memory available\: %.0lf%s bytes" \
-		GPRINT:totalswap:MIN:"Total swap available\: %.0lf%s bytes" \
-		COMMENT:"	\j"
+	graph|graph-day)  do_graph day
 		;;
-	(graph-weekly)
-	    rrdtool graph ${GRAPHNAME//.png/-week.png} \
-		-v "Bytes" -w 700 -h 300 -t "${MYHOST} last 7 days memory usage- ${DATE}" \
-		--end now --start end-$LASTWEEK -c ARROW\#000000  \
-		DEF:usedmem=${RRDFILE}:usedmem:AVERAGE \
-		DEF:usedswap=${RRDFILE}:usedswap:AVERAGE \
-		DEF:totalmem=${RRDFILE}:totalmem:AVERAGE \
-		DEF:totalswap=${RRDFILE}:totalswap:AVERAGE \
-		COMMENT:"	" \
-		AREA:totalmem\#0000FF:"Total phys memory" \
-		AREA:totalswap\#000000:"Total with swap":STACK \
-		AREA:usedmem\#00FF00:"Memory used" \
-		LINE:usedswap\#FF0000:"Swap used":STACK \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:usedmem:MAX:"Maximum memory used\: %.0lf%s bytes" \
-		GPRINT:usedswap:MAX:"Maximum swap used\: %.0lf%s bytes" \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:totalmem:MAX:"Total memory available\: %.0lf%s bytes" \
-		GPRINT:totalswap:MIN:"Total swap available\: %.0lf%s bytes" \
-		COMMENT:"	\j"
+	graph-weekly)   do_graph week
 		;;
-	(graph-monthly)
-	    rrdtool graph ${GRAPHNAME//.png/-month.png} \
-		-v "Bytes" -w 700 -h 300 -t "${MYHOST} last month's memory usage- ${DATE}" \
-		--end now --start end-$LASTMONTH -c ARROW\#000000  \
-		DEF:usedmem=${RRDFILE}:usedmem:AVERAGE \
-		DEF:usedswap=${RRDFILE}:usedswap:AVERAGE \
-		DEF:totalmem=${RRDFILE}:totalmem:AVERAGE \
-		DEF:totalswap=${RRDFILE}:totalswap:AVERAGE \
-		COMMENT:"	" \
-		AREA:totalmem\#0000FF:"Total phys memory" \
-		AREA:totalswap\#000000:"Total with swap":STACK \
-		AREA:usedmem\#00FF00:"Memory used" \
-		LINE:usedswap\#FF0000:"Swap used":STACK \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:usedmem:MAX:"Maximum memory used\: %.0lf%s bytes" \
-		GPRINT:usedswap:MAX:"Maximum swap used\: %.0lf%s bytes" \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:totalmem:MAX:"Total memory available\: %.0lf%s bytes" \
-		GPRINT:totalswap:MIN:"Total swap available\: %.0lf%s bytes" \
-		COMMENT:"	\j"
+	graph-monthly)  do_graph month
 		;;
-	(graph-yearly)
-	    rrdtool graph ${GRAPHNAME//.png/-year.png} \
-		-v "Bytes" -w 700 -h 300 -t "${MYHOST} last year's memory usage- ${DATE}" \
-		--end now --start end-$LASTYEAR -c ARROW\#000000  \
-		DEF:usedmem=${RRDFILE}:usedmem:AVERAGE \
-		DEF:usedswap=${RRDFILE}:usedswap:AVERAGE \
-		DEF:totalmem=${RRDFILE}:totalmem:AVERAGE \
-		DEF:totalswap=${RRDFILE}:totalswap:AVERAGE \
-		COMMENT:"	" \
-		AREA:totalmem\#0000FF:"Total phys memory" \
-		AREA:totalswap\#000000:"Total with swap":STACK \
-		AREA:usedmem\#00FF00:"Memory used" \
-		LINE:usedswap\#FF0000:"Swap used":STACK \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:usedmem:MAX:"Maximum memory used\: %.0lf%s bytes" \
-		GPRINT:usedswap:MAX:"Maximum swap used\: %.0lf%s bytes" \
-		COMMENT:"	\j" \
-		COMMENT:"	" \
-		GPRINT:totalmem:MAX:"Total memory available\: %.0lf%s bytes" \
-		GPRINT:totalswap:MIN:"Total swap available\: %.0lf%s bytes" \
-		COMMENT:"	\j"
+	graph-yearly)   do_graph year
+		;;
+	graph-all) for i in day week month year; do
+			echo "dbg: graphing ${i}"
+			do_graph ${i}
+		   done
 		;;
 	(*)
 		echo "Invalid option for ${PROGNAME}"
